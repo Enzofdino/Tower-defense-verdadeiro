ac117b637d1efd4a00b700d84357cbfef3fd94cf
Enzofdino <enzofhostin@gmail.com>
2024-11-13 14:58:53 -0300

    começando a colocar a propaganda
    mas so estou começando mas n está funcionando

---
+++ Defesa da torre(tanques)/Assets/Script/Propaganda.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour,IUnityAdsInitializationListener
+{
+    [SerializeField] private string androidGameId;
+    [SerializeField] private string iosGameId;
+    [SerializeField] private bool isTesting;
+
+    private string gameId;
+
+    
+    private void Awake()
+    {
+      #if UNITY_IOS
+      gameId = iosGameId;
+      #elif UNITY_ANDROID
+      gameId = androidGameId;
+      #elif UNITY_EDITOR
+      gameId = androidGameId;
+      #endif
+
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(gameId, isTesting, this);
+        }
+
+        public void OnInitializationComplete()
+        {
+            Debug.Log("Ads Initialized");
+        }
+
+        public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+        {
+          
+        }
+
+
+
+
+    }
+
+}
